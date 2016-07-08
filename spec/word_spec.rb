cc89cd8108e54c34d7cfd5fa151d22a460e64817
BrianHGrant <bhgrant8@gmail.com>
2016-07-08 08:41:51 -0700

    add passing specs for Word#id and Word#definitions methods

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,4 +8,16 @@ describe('Word') do
       expect(test_word.term()).to(eq("anneal"))
     end
   end
+  describe('#term') do
+    it('will return the id when term entered') do
+      test_word = Word.new(:term => "anneal")
+      expect(test_word.id()).to(eq(1))
+    end
+  end
+  describe('#term') do
+    it('will return the definitions when term entered') do
+      test_word = Word.new(:term => "anneal")
+      expect(test_word.definitions()).to(eq([]))
+    end
+  end
 end
