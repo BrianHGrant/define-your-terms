18df83c4f1adce5cbcdd1331052a2db163bd1b89
BrianHGrant <bhgrant8@gmail.com>
2016-07-08 10:20:15 -0700

    add additional passing specs for all paramater returns for instances of Definition class

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,6 +2,12 @@ require('rspec')
 require('definition')
 
 describe('Definition') do
+  describe('#def') do
+    it('will return the definition when entered') do
+      test_definition = Definition.new(:part => 'verb', :def => 'heat (metal or glass) and allow it to cool slowly, in order to remove internal stresses and toughen it.')
+      expect(test_definition.def()).to(eq('heat (metal or glass) and allow it to cool slowly, in order to remove internal stresses and toughen it.'))
+    end
+  end
   describe('#id') do
     it('will return the id when definition entered') do
       test_definition = Definition.new(:part => 'verb', :def => 'heat (metal or glass) and allow it to cool slowly, in order to remove internal stresses and toughen it.')
