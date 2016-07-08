0f0a1ee95a2b4bc885b95a94e718dc67764c2f75
BrianHGrant <bhgrant8@gmail.com>
2016-07-08 15:53:48 -0700

    finish ui and add removal

--- app.rb
+++ app.rb
@@ -9,14 +9,60 @@ get('/') do
   erb(:index)
 end
 
-post('/words/new') do
+get('/words/new') do
   erb(:words_form)
+end
+
+get('/words/remove') do
+  @words = Word.all()
+  erb(:words_remove_form)
+end
+
+get('/words/remove/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  Word.remove(@word.id().to_i())
+  @words = Word.all()
+  erb(:words_remove_form)
 end
 
 post('/words') do
   term = params.fetch('term')
-  @new_word = Word.new(:term => term)
-  @new_word.save()
+  Word.new(:term => term).save()
   @words = Word.all()
   erb(:index)
+end
+
+get('/words/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  @definitions = @word.definitions()
+  erb(:word)
+end
+
+get('/words/:id/definitions/new') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:word_definitions_form)
+end
+
+get('/words/:id/definitions/remove') do
+  @word = Word.find(params.fetch('id').to_i())
+  @definitions = @word.definitions()
+  erb(:word_definitions_remove_form)
+end
+
+get ('/words/:word_id/definitions/remove/:definition_id') do
+  @definitions = Word.find(params.fetch('id').to_i()).definitions()
+  @definition = Definition.find(params.fetch('id').to_i())
+  Definition.remove(@definition.id().to_i())
+  erb(:word_definitions_remove_form)
+end
+
+post('/definitions') do
+  part = params.fetch('part')
+  definition = params.fetch('definition')
+  @definition = Definition.new(:part => part, :def => definition)
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@definition)
+  @definitions = @word.definitions()
+  erb(:word)
 end
